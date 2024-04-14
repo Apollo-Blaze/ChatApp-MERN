bf24b771541acea80ce456fc3a21fab185394b80
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-04-14 21:44:07 +0530

    signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/usermodel.js";
+
 export const login=(req,res)=>{
     console.log("loginUser");
 }
@@ -7,8 +9,33 @@ export const logout=(req,res)=>{
 export const signup=async (req,res)=>{
     try{
         const {fullName,username,password,confirmPassword,gender} =req.body;
-    }
-    catch(error){
+        if(password!==confirmPassword){
+            return res.status(400),json({error:"Passwords dont match"})
+        }
+        const user=await User.findOne({username});
+        if(user){
+            return res.status(400),json({error:"Username already exists"})
+        }
+        const boyProfilePic=`https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic=`https://avatar.iran.liara.run/public/girl?username=${username}`
 
+        const newUser=new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender==="male"?boyProfilePic:girlProfilePic
+        })
+        await newUser.save();
+        res.statuse(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            username:newUser.username,
+            profilePic:newUser.profilePic
+        })
+    }   
+    catch(error){
+        console.log("Error in signup controller")
+        res.status(500),json({error:"Internal server error"})
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+
+
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connection.js";
 
 const app=express()
+const PORT=process.env.PORT || 5000;
 
 dotenv.config();
-const PORT=process.env.PORT || 5000
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Server is ready")
-});
+// app.get("/",(req,res)=>{
+//     res.send("Server is ready")
+// });
 
 app.use("/api/auth",authRoutes)
 
